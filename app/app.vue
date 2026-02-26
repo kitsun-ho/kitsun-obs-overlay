7d9954ff3a66c4ab0f3fffb64cdef673afa8ac2d
ken.ho <ken.ho@ecloudvalley.com>
2026-02-26 07:04:57 +0000

    Test. test

--- app/app.vue
+++ app/app.vue
@@ -2,5 +2,9 @@
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    <div class="m-4">
+      <BaseButton>Primary Button</BaseButton>
+      <BaseButton secondary>Secondary Button</BaseButton>
+    </div>
   </div>
 </template>
